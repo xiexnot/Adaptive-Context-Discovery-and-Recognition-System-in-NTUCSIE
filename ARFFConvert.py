19c77e1a776dc6fc617c8d0db222899639975d64
xiexnot <xiexnot@gmail.com>
2016-05-18 18:04:29 +0800

    Finish ARFF_Convert.py in initial version

--- ARFFConvert.py
+++ ARFFConvert.py
@@ -11,6 +11,7 @@ import os
 import sys
 import math
 from tools import read_dataset
+import json
 
 """
 ----------------------------------------------
@@ -25,7 +26,7 @@ def Instance2ARFF(data, label):
 	result = ""
 	for i in range(len(data)):
 		result = result + str(data[i]) + ','
-	result += ('Context#' + label)
+	result += label
 	return result
 
 """
@@ -50,12 +51,71 @@ def DataPreprocessing(totaldata):
 """
 ----------------------------------------------
 	InputData
-	Description:	Only process the nomimal value to nominal value.
-	Hints:	Not normalization here.
+	Description:	Get the Input Data
+	Hints:	
 ----------------------------------------------
 """
 
-def InputData(totaldata, totallabel, configure):
+def InputData(filename, totaldata, totallabel, configure):
+	FILE = open(filename,'rU')
+	rawdata = FILE.read()
+	decoded = json.loads(rawdata)
+	FILE.close()
+	print decoded["totaldata_filename"]
+	print decoded["totallabel_filename"]
+	print decoded["configure_filename"]
+	print decoded["ARFF_filename"]
+	#read data
+	totaldata = read_dataset(decoded["totaldata_filename"],"\t")
+	#read label
+	FILE_label = open(decoded["totallabel_filename"],'rU')
+	totallabel = FILE_label.read()
+	totallabel = totallabel.split('\n')
+	if len(totallabel[totallabel.__len__()]) == 0:
+		totallabel = totallabel[:-1]
+	FILE_label.close()
+	#read configure in each parameter
+	FILE_configure = open(decoded["configure_filename"],'rU')
+	configure = FILE_configure.read()
+	configure = configure.split('\n')
+	if len(configure[configure.__len__()]) == 0:
+		configure = configure[:-1]
+	FILE_configure.close()
+	return 0, decoded["ARFF_output_filename"]
 
+"""
+----------------------------------------------
+	ARFFHeaderPrint
+	Description:	Print the header of ARFF
+	Hints:	
+---------------------------------------------
+"""
+
+def ARFFHeaderPrint(ARFF_Output, configure):
+
+	ARFF_Output.write('@RELATION\tinitial\tmodel\n')
+	for i in range(len(configure)):
+		ARFF_Output.write('@ATTRIBUTE\t'+configure[i]+'\t'+'REAL\n')
+	ARFF_Output.write('@ATTRIBUTE\tclass\tinteger\n')
+	ARFF_Output.write('@DATA\n')
+	
 	return 0
+	
+totaldata = []
+totallabel = []
+configure = []
+
+try:
+	status, ARFF_output_filename = InputData(sys.argv[1], totaldata, totallabel, configure)
+	DataPreprocessing(totaldata)
+	ARFF_Output = open(ARFF_output_filename,'w')
+	ARFFHeaderPrint(ARFF_Output, configure)
+	for i in range(len(totaldata)):
+		ARFF_Output.write(Instance2ARFF(totaldata[i], label[i]))
+		ARFF_Output.write('\n')
+		pass
+	ARFF_Output.close()
+except:
+	exit()
+	
 
