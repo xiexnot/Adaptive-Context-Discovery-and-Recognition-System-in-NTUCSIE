d488bf6823c839f488329b31b9c8486ed5089fce
xiexnot <xiexnot@gmail.com>
2016-05-25 17:33:42 +0800

    last version of updating AR Component based on ARFF

--- MARCS_Recognition.py
+++ MARCS_Recognition.py
@@ -82,12 +82,30 @@ def print_json_to_file(filename, dict_data):
 	FILE.write(dict_data)
 	FILE.close()
 	return 0
-
-def ConvertInstance2ARFF(instance):
-	pass
+"""
+def ConvertInstance2ARFF(AR_instance, ARFF_header_filename, configure):
+	#print data to temporary external file
+	FILE = open(ARFF_header_filename, 'rU')
+	header = FILE.read()
+	FILE.close()
+	FILE = open('MARCS_AR_temporary_log','w')
+	FILE.write(header)
+	for i in range(len(AR_instance)):
+		for j in range(len(AR_instance[i])):
+			if 'on' in AR_instance[i][j]:
+				AR_instance[i][j] = '1'
+			elif 'off' in AR_instance[i][j]:
+				AR_instance[i][j] = '0'
+			elif 'stand' in AR_instance[i][j]:
+				AR_instance[i][j] = '0.1'
+			if configure[j][1] == integer:
+			elif
+	
+	
+	FILE.close()
 	loader = Loader(classname="weka.core.converters.ArffLoader")
 	return 0
-
+"""
 """
 ----------------------------------------------
 	ActivityRecognition
@@ -97,11 +115,11 @@ def ConvertInstance2ARFF(instance):
 ----------------------------------------------
 """
 
-def ActivityRecognition(AR_filename, WL_filename, Semantic_filename):
+def ActivityRecognition(AR_filename, WL_filename, Semantic_filename, ARFF_header_filename, configure):
 	pass
 	#read the file from AR_filename
 	AR_instance = read_dataset(AR_filename,'\t')
-	AR_instance_ARFF = ConvertInstance2ARFF(AR_instance)
+	AR_instance_ARFF = ConvertInstance2ARFF(AR_instance, ARFF_header_filename, configure)
 	#read the semantic meaning from extrenal file
 	Semantic_Meaning = read_json(Semantic_filename)
 
@@ -115,7 +133,6 @@ def ActivityRecognition(AR_filename, WL_filename, Semantic_filename):
 			PrintInstanceWL(AR_instance[i],WL_filename)
 		else:
 			if Semantic_Meaning.has_key(str(is_familar_pattern)) == True:
-			#if str(is_familar_pattern) in Semantic_Meaning:
 				#find propable semantic meaning
 				print "AR Result: " + Semantic_Meaning[str(is_familar_pattern)]
 			else:
@@ -141,23 +158,34 @@ def Initialization(filename, Initial_ARFF):
 	
 	FILE = open(filename,'rU')
 	rawdata = FILE.read()
+	FILE.close()
 	#print rawdata
 	decoded = json.loads(rawdata)
 	print decoded['Initial_ARFF_filename']
+	print decoded['configure_filename']
+
 	Initial_ARFF_FILE = open(decoded['Initial_ARFF_filename'],'rU')
 	Initial_ARFF = Initial_ARFF_FILE.read()
 	Initial_ARFF_FILE.close() 
+
+	FILE = open(decoded['configure_filename'],'rU')
+	configure = FILE.read()
+	configure = configure.split('\n')
+	for i in range(configure.__len__()):
+		configure[i] = configure[i].split('\t')
 	FILE.close()
-	return decoded['log_filename'], decoded['WL_filename'], decoded['Semantic_filename'], decoded['ARFF_header_filename']
+	
+	return decoded['log_filename'], decoded['WL_filename'], decoded['Semantic_filename'], decoded['ARFF_header_filename'], configure
 
 def main():
 
 	Initial_ARFF = []
-	log_filename, WL_filename, Semantic_filename, ARFF_header_filename = Initialization(sys.argv[1], Initial_ARFF)
+	log_filename, WL_filename, Semantic_filename, ARFF_header_filename, configure = Initialization(sys.argv[1], Initial_ARFF)
 	print 'log_filename = ',log_filename
 	print 'WaitingList_filename = ', WL_filename
 	print 'Semantic_filename = ', Semantic_filename
 	print "ARFF_header_filename = ", ARFF_header_filename
+	print "configure = ", configure
 
 	while True:
 		command = raw_input('\nADD:ADD [ARFF\'s filename]\nAR:AR [raw data of dataset\' filename]\nplease enter the command:')
@@ -171,7 +199,7 @@ def main():
 
 		if command[0] == 'AR' or command[0] == 'ar':
 			print "MARCS Activity Recognition...loading..."
-			ActivityRecognition(command[1], WL_filename, Semantic_filename)
+			ActivityRecognition(command[1], WL_filename, Semantic_filename, ARFF_header_filename, configure)
 			# Input File: like BL313's dataset
 			print "MARCS Activity Recognition...finished."
 			pass
