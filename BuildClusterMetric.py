eb1f1599dfdfd294a9c2ebf7a3f2cef61f401e51
YIfei Xie <xiexnot@gmail.com>
2016-05-30 22:40:09 +0800

    update def_main for BuildClusterMetric

--- BuildClusterMetric.py
+++ BuildClusterMetric.py
@@ -18,90 +18,95 @@ from sklearn.neighbors.nearest_centroid import NearestCentroid
 from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
 import numpy as np
 
-FILE = open(sys.argv[1],'rU')
-rawdata = FILE.read()
-decoded = json.loads(rawdata)
-FILE.close()
-print decoded["clustering_filename"]
-print decoded["dataset_filename"]
-print decoded["metric_filename"]
+def main(input_json_filename):
 
-instance = read_dataset(decoded["dataset_filename"],'\t')
+	FILE = open(input_json_filename,'rU')
+	rawdata = FILE.read()
+	decoded = json.loads(rawdata)
+	FILE.close()
 
-FILE = open(decoded["clustering_filename"],'rU')
-clustering = FILE.read()
-clustering = clustering.split('\n')
-if clustering[clustering.__len__()-1]:
-	clustering = clustering[:-1]
-FILE.close()
+	print decoded["clustering_filename"]
+	print decoded["dataset_filename"]
+	print decoded["metric_filename"]
 
-for i in range(instance.__len__()):
-	for j in range(len(instance[i])):
-		if 'on' in instance[i][j]:
-			instance[i][j] = '1'
-		elif 'off' in instance[i][j]:
-			instance[i][j] = '0'
-		elif 'stand' in instance[i][j]:
-			instance[i][j] = '0.1'
+	instance = read_dataset(decoded["dataset_filename"],'\t')
 
-		instance[i][j] = float(instance[i][j])
+	FILE = open(decoded["clustering_filename"],'rU')
+	clustering = FILE.read()
+	clustering = clustering.split('\n')
+	if clustering[clustering.__len__()-1]:
+		clustering = clustering[:-1]
+	FILE.close()
 
-for i in range(clustering.__len__()):
-	clustering[i] = int(clustering[i])
+	for i in range(instance.__len__()):
+		for j in range(len(instance[i])):
+			if 'on' in instance[i][j]:
+				instance[i][j] = '1'
+			elif 'off' in instance[i][j]:
+				instance[i][j] = '0'
+			elif 'stand' in instance[i][j]:
+				instance[i][j] = '0.1'
 
-max_no = max(clustering)
+			instance[i][j] = float(instance[i][j])
 
-clustering_head = []
-for cluster_item in range(max_no+1):
-	item = []
-	for i in range(len(instance[0])):
-		item.append(0.0)
-	count = 0
 	for i in range(clustering.__len__()):
-		if clustering[i] == cluster_item:
-			count += 1
-			for j in range(len(instance[i])):
-				item[j] += instance[i][j]
-	for i in range(item.__len__()):
-		item[i] = round(item[i] / float(count), 2)
-	clustering_head.append(item)
+		clustering[i] = int(clustering[i])
 
-FILE = open(decoded["metric_filename"],'w')
-print clustering_head
-for i in range(clustering_head.__len__()):
-	if i!=0:
-		FILE.write('\n')
-	for j in range(len(clustering_head[i])):
-		if j != 0:
-			FILE.write('\t')
-		FILE.write(str(clustering_head[i][j]))
-FILE.close()
+	max_no = max(clustering)
+
+	clustering_head = []
+	for cluster_item in range(max_no+1):
+		item = []
+		for i in range(len(instance[0])):
+			item.append(0.0)
+		count = 0
+		for i in range(clustering.__len__()):
+			if clustering[i] == cluster_item:
+				count += 1
+				for j in range(len(instance[i])):
+					item[j] += instance[i][j]
+		for i in range(item.__len__()):
+			item[i] = round(item[i] / float(count), 2)
+		clustering_head.append(item)
+
+	FILE = open(decoded["metric_filename"],'w')
+	print clustering_head
+	for i in range(clustering_head.__len__()):
+		if i!=0:
+			FILE.write('\n')
+		for j in range(len(clustering_head[i])):
+			if j != 0:
+				FILE.write('\t')
+			FILE.write(str(clustering_head[i][j]))
+	FILE.close()
 
 ######################################
 
-clustering_head_label = []
-for i in range(clustering_head.__len__()):
-	clustering_head_label.append(i)
+	clustering_head_label = []
+	for i in range(clustering_head.__len__()):
+		clustering_head_label.append(i)
 
 #clf = svm.SVC(probability = True)
 #clf = tree.DecisionTreeClassifier()
-clf = tree.ExtraTreeClassifier()
-clf = clf.fit(clustering_head, clustering_head_label)
+	clf = tree.ExtraTreeClassifier()
+	clf = clf.fit(clustering_head, clustering_head_label)
 
-FILE = open('BL313_test_instance','rU')
-test = FILE.read()
-test = test.split('\t')
-print test
-for i in range(test.__len__()):
-	if 'on' in test[i]:
+	FILE = open('BL313_test_instance','rU')
+	test = FILE.read()
+	test = test.split('\t')
+	print test
+	for i in range(test.__len__()):
+		if 'on' in test[i]:
 			test[i] = '1'
-	elif 'off' in test[i]:
+		elif 'off' in test[i]:
 			test[i] = '0'
-	elif 'stand' in test[i]:
+		elif 'stand' in test[i]:
 			test[i] = '0.1'
-	
-	test[i] = float(test[i])
-test = [test]
-FILE.close()
+		test[i] = float(test[i])
+	test = [test]
+	FILE.close()
 
-print clf.predict_proba(test)
+	print clf.predict_proba(test)
+
+if __name__ == "__main__":
+	main()
